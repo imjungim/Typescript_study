c9b6cd59c44a736c91a725a37d68c312136bef92
imjungim <jungim4201@gmail.com>
2023-03-02 22:54:46 +0900

    4.1 dictionary 예제

---
+++ TS_BlockChain/class&interface/4.1 Recap.ts
@@ -0,0 +1,37 @@
+//단어를 추가하고, 찾기, 삭제하는 메소드 생성
+type Words = {
+  //object의 type선언 property에대해 type만 알때 사용
+  [key:string] : string
+}
+
+// let dict: Words = {
+//   "potato": "food"//type을 string에 맞춰사용
+// }
+
+class Dict {
+  private words: Words
+  constructor() {
+    this.words = {}
+  }
+  add(word: Word) { //Word class를 type 사용
+    if (this.words[word.term] === undefined) { //주어진 단어가 아직 사전에 존재하지 않음
+      this.words[word.term] === word.def
+    }
+  }
+  def(term: string) {
+    return this.words[term]
+  }
+}
+
+class Word {
+  constructor(
+    public term: string,
+    public def : string
+  ){}
+}
+
+const kimchi = new Word("kimchi", "한국 음식");
+
+const dict = new Dict()
+dict.add(kimchi);
+dict.def("kimchi")
