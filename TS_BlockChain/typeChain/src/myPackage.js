be32ea8c18baa9a875a0ae35156235569ba8a4f5
imjungim <jungim4201@gmail.com>
2023-03-14 19:18:09 +0900

    5.4 JSDoc

--- TS_BlockChain/typeChain/src/myPackage.js
+++ TS_BlockChain/typeChain/src/myPackage.js
@@ -1,6 +1,22 @@
+//@ts-check
+// javascript 파일 내에서 타입 검사를 허용(ts가 js파일도 보호)
+
+/**
+* @param {object} config
+* @param {boolean} config.debug
+* @param {string} config.url 
+* @returns boolean 
+*/
+
 export function init(config) {
   return true
 }
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns number
+ */
 
 export function exit(code) {
   return code + 1;
