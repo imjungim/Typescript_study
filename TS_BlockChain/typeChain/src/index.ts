beb5e8ea60ef810ff40ccab7224184e57cd1185c
imjungim <jungim4201@gmail.com>
2023-03-14 14:00:53 +0900

    5.3 declaration Files

--- TS_BlockChain/typeChain/src/index.ts
+++ TS_BlockChain/typeChain/src/index.ts
@@ -1,10 +1,13 @@
-const hellooo = () => 'hi';
+//myPackage가 node의 모듈인것처럼 사용 (npm 설치했다는 가정)
 
-class Block { 
-  constructor(
-    private data:string
-  ) { }
-  static hello() {
-    return "hi";
-  }
-}
+//myPackage.d.ts에 타입을 정의 -> 타입스크립트가 init의 타입에 대해 알고 있기 때문에 더이상 에러 없음.
+import { init, exit } from "myPackage";
+
+init({
+  url:'true'
+})
+
+exit(2)
+
+//ts가 d.ts파일을 읽어서 타입을 인지
+localStorage
---
+++ TS_BlockChain/typeChain/src/myPackage.js
@@ -0,0 +1,9 @@
+export function init(config) {
+  return true
+}
+
+export function exit(code) {
+  return code + 1;
+}
+
+//only js code -> typeScript는 이게 뭔지 모름.
