ae2c4a5c2ab3840fabefe378a8978e2b152a5b04
imjungim <jungim4201@gmail.com>
2023-03-31 19:18:26 +0900

    3-5-1 CafeLatteMachine class 생성

---
+++ TS_DreamCoding/3-oop/3-5-inheritance.ts
@@ -0,0 +1,135 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  }
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CafeLatteMachine {
+    private static BEANS_GRAMM_PER_SHOT: number = 7 
+    private coffeeBeans: number = 0; 
+
+    private constructor(coffeeBeans: number) { 
+    this.coffeeBeans = coffeeBeans
+    }
+    
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+        return new CoffeeMachine(coffeeBeans)
+    }
+    
+    fillCoffeeBeans(beans: number) {
+    //커피추가
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans
+    }
+
+    clean() {
+      console.log('cleaning the machine...')
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`)
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!')
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up...')
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots...☕`)
+      return {
+      shots,
+      hasMilk: false,
+      }
+      
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      const coffee = this.extract(shots);
+      return { ...coffee, hasMilk: true };
+
+    }
+  }
+
+  class CoffeeMachine implements CoffeeMaker  {
+    private static BEANS_GRAMM_PER_SHOT: number = 7 
+    private coffeeBeans: number = 0; 
+
+    private constructor(coffeeBeans: number) { 
+    this.coffeeBeans = coffeeBeans
+    }
+    
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+        return new CoffeeMachine(coffeeBeans)
+    }
+    
+    fillCoffeeBeans(beans: number) {
+    //커피추가
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans
+    }
+
+    clean() {
+      console.log('cleaning the machine...')
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`)
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!')
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up...')
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots...☕`)
+      return {
+      shots,
+      hasMilk: false,
+      }
+      
+    }
+    // ❗interface 함수 구현
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+
+    }
+  }
+
+  const maker:CoffeeMachine = CoffeeMachine.makeMachine(32)
+  maker.fillCoffeeBeans(32)
+  maker.makeCoffee(2)
+  maker.makeCoffee(2)
+  maker.fillCoffeeBeans(20)
+  
+  //2. interface 사용
+  // const maker2:CoffeeMaker = CoffeeMachine.makeMachine(32)
+  // maker2.fillCoffeeBeans(32) 
+  // maker2.makeCoffee(2)
+  
+  // const maker2:CommercialCoffeeMaker = CoffeeMachine.makeMachine(32)
+  // maker2.fillCoffeeBeans(32) 
+  // maker2.makeCoffee(2)
+  // maker2.clean();
+
+
+
+}
