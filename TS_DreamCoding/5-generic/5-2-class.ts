09eb1fb12b1d049e1db257053ae4715b2a4bbf2e
imjungim <jungim4201@gmail.com>
2023-04-04 15:35:05 +0900

    SimpleEither class, Either interface 제네릭 적용

--- TS_DreamCoding/5-generic/5-2-class.ts
+++ TS_DreamCoding/5-generic/5-2-class.ts
@@ -1,22 +1,29 @@
 //either : a or b
 //이거 아님 저거
-interface Either {
-  left: () => number;
-  right: () => number;
+
+//숫자가 아닌 어떤 타입이든 담을 수 있도록 -> 제네릭 활용
+//왼쪽, 오른쪽이 서로 타른 타입일 수도??
+interface Either<L, R> {
+  left: () => L;
+  right: () => R;
 }
 
-class SimpleEither implements Either {
-  constructor(private leftValue: number, private rightValue: number) { }
+class SimpleEither<L, R> implements Either<L, R> {
+  constructor(private leftValue: L, private rightValue: R) { }
   
-  left(): number{
+  left(): L{
     return this.leftValue;
   }
 
-  right(): number{
+  right(): R{
     return this.rightValue;
   }
 }
 
-const either = new SimpleEither(4, 5);
+const either: Either<number, number> = new SimpleEither(4, 5);
 either.left() //4
 either.right() //5
+
+const best = new SimpleEither(4, 'hello'); //숫자, 문자 
+const best2 = new SimpleEither({name: 'hong'}, 'hello'); //object타입도 가능
+
